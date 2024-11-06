ca667927e879ef350dd7d016242975020d99884e
Vkanhan <kanhanvishnu@gmail.com>
2024-11-06 16:20:14 +0530

    check if the array has duplicate numbers

--- leetcode/arrayAndHashing/hasDuplicate/main.go
+++ leetcode/arrayAndHashing/hasDuplicate/main.go
@@ -6,19 +6,14 @@ import(
 
 //hasDuplicate checks if there is a duplicate int in the slice of array
 func hasDuplicate(nums []int) bool {
-	//Initialize a map to store the unique element encountered
 	hashSet := make(map[int]bool)
 	
-	//Iterate over each element
 	for _, num := range nums {
-		//Check if the element is already in the map
 		if hashSet[num] {
 			return true
 		}
-		//Otherwise add element to the map
 		hashSet[num] = true
 	}
-	//Return false if no duplicates
 	return false
 }
 
